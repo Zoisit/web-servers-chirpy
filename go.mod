9b5b54c8f28f07e90291c605c35aece68be1dce1
Zoisit <smaragdeule@gmx.de>
2025-03-02 14:09:01 +0100

    C1-L5: Local server with index file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Zoisit/web-servers-chirpy
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	//serveMux.Handle("/", http.FileServer())
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: serveMux,
+	}
+
+	server.ListenAndServe()
+}
