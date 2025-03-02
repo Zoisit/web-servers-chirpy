b0816450c693f902b7ed525b1c0934cda42db81c
Zoisit <smaragdeule@gmx.de>
2025-03-02 17:05:12 +0100

    CH1: Add logo and readiness check

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 func main() {
 	serveMux := http.NewServeMux()
-	//serveMux.Handle("/", http.FileServer())
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	serveMux.Handle("/", http.HandlerFunc(handlerReadyCheck))
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -15,4 +15,10 @@ func main() {
 	}
 
 	server.ListenAndServe()
+}
+
+func handlerReadyCheck(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	rw.Write([]byte("OK"))
 }
