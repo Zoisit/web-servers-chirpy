86796f743d2febfaeeafedfbd1e93a518a401fb9
Zoisit <smaragdeule@gmx.de>
2025-08-21 15:49:20 +0200

    Current state

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	apiCfg := apiConfig{}
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	serveMux.Handle("/", http.HandlerFunc(handlerReadyCheck))
+	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", apiCfg.middlewareMetricsInc(appHandler))
+	serveMux.Handle("/", apiCfg.middlewareMetricsInc(http.HandlerFunc(handlerReadyCheck)))
+	serveMux.Handle("/metrics", http.HandlerFunc(apiCfg.handlerGetApiHits))
+	serveMux.Handle("/reset", http.HandlerFunc(apiCfg.handlerResetApiHits))
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -21,4 +31,26 @@ func handlerReadyCheck(rw http.ResponseWriter, req *http.Request) {
 	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	rw.WriteHeader(200)
 	rw.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerGetApiHits(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	rw.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerResetApiHits(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+	rw.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+		cfg.fileserverHits.Add(1)
+	}
+
+	return http.HandlerFunc(f)
 }
